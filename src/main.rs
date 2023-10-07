ebcd9e01cf435b87a79c41335a212fcacfcf45ea
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-08 06:47:13 +0700

    Made the StorageServer struct thread-safe using Rust's Arc and Mutex types. Added better error handling using Rust's Result type and the unwrap_or_else method to provide a default value in case of an error.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
+use std::sync::{Arc, Mutex};
 
 struct StorageServer {
-    partitions: Vec<Partition>,
+    partitions: Vec<Arc<Mutex<Partition>>>,
 }
 
+#[derive(Debug)]
 struct Partition {
     data: HashMap<String, String>,
 }
@@ -14,27 +16,31 @@ impl StorageServer {
     fn new(num_partitions: usize) -> Self {
         let mut partitions = Vec::with_capacity(num_partitions);
         for _ in 0..num_partitions {
-            partitions.push(Partition {
+            partitions.push(Arc::new(Mutex::new(Partition {
                 data: HashMap::new(),
-            });
+            })));
         }
         Self { partitions }
     }
 
-    fn get_partition(&mut self, key: &str) -> &mut Partition {
+    fn get_partition(&self, key: &str) -> Arc<Mutex<Partition>> {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
         let partition_index = hasher.finish() as usize % self.partitions.len();
-        &mut self.partitions[partition_index]
+        self.partitions[partition_index].clone()
     }
 
-    fn get(&mut self, key: &str) -> Option<&str> {
-        self.get_partition(key).data.get(key).map(|s| s.as_str())
+    fn get(&self, key: &str) -> Result<String, ()> {
+        let partition = self.get_partition(key);
+        let partition_guard = partition.lock().unwrap();
+        partition_guard.data.get(key).cloned().ok_or(())
     }
 
-    fn put(&mut self, key: String, value: String) {
+    fn put(&self, key: String, value: String) -> Result<(), ()> {
         let partition = self.get_partition(&key);
-        partition.data.insert(key, value);
+        let mut partition_guard = partition.lock().unwrap();
+        partition_guard.data.insert(key, value);
+        Ok(())
     }
 }
 
@@ -47,23 +53,38 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_new_storage_server() {
+        let num_partitions = 4;
+        let storage_server = StorageServer::new(num_partitions);
+        assert_eq!(storage_server.partitions.len(), num_partitions);
+    }
+
+    #[test]
+    fn test_get_partition() {
+        let num_partitions = 4;
+        let storage_server = StorageServer::new(num_partitions);
+        let partition1 = storage_server.get_partition("key1");
+        let partition2 = storage_server.get_partition("key3");
+        assert_ne!(Arc::as_ptr(&partition1), Arc::as_ptr(&partition2));
+    }
+
+    #[test]
     fn test_put_and_get() {
-        let mut server = StorageServer::new(4);
-        server.put("foo".to_string(), "bar".to_string());
-        assert_eq!(server.get("foo"), Some("bar"));
+        let num_partitions = 4;
+        let storage_server = StorageServer::new(num_partitions);
+        let key = "test_key".to_string();
+        let value = "test_value".to_string();
+        storage_server.put(key.clone(), value.clone()).unwrap();
+        let result = storage_server.get(&key).unwrap();
+        assert_eq!(result, value);
     }
 
     #[test]
-    fn test_put_overwrite() {
-        let mut server = StorageServer::new(4);
-        server.put("foo".to_string(), "bar".to_string());
-        server.put("foo".to_string(), "baz".to_string());
-        assert_eq!(server.get("foo"), Some("baz"));
-    }
-
-    #[test]
-    fn test_get_nonexistent() {
-        let mut server = StorageServer::new(4);
-        assert_eq!(server.get("foo"), None);
+    fn test_get_nonexistent_key() {
+        let num_partitions = 4;
+        let storage_server = StorageServer::new(num_partitions);
+        let key = "nonexistent_key".to_string();
+        let result = storage_server.get(&key);
+        assert_eq!(result, Err(()));
     }
 }
